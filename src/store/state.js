4b67cd582c57fcdfcb8c427556a4214706ea8dd2
iteye <iteyelmp@gmail.com>
2025-11-11 18:05:46 +0800

    change network

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,6 @@
 export const chains = [{
-  chainID: '0xaa36a7',
-  Conversion: '0xE4c8f92f239f465Fa0A4981270457222303399da',
-  OldToken: '0xBf0b6e5C39d4afECB824305397729cd0493792E7',
-  L2Rpc: 'https://rpc.gamma.testnet.l2.quarkchain.io:8545'
+  chainID: '0xaa36a7', // 11155111
+  Conversion: '0x117be5BFF6b6424D4Be658BFFdDF5C897663A49e',
+  OldToken: '0xC359FCF9328143f798C197B86856e656411aBC48',
+  L2Rpc: 'https://rpc.delta.testnet.l2.quarkchain.io:8545'
 }];
